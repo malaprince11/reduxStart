b6c7ca2162687bd353e25be2c716cf96e7551edd
malaprince <malaminebah91@gmail.com>
2020-09-11 14:54:13 +0200

    done

--- src/Compteur/Compteur.js
+++ src/Compteur/Compteur.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 import './Compteur.css';
 import Resultat from '../Composants/Resultat/Resultat.js';
 import IncrBtn from '../Composants/IncrBtn/IncrBtn.js';
+import {connect} from 'react-redux'
 
 class Compteur extends Component {
 
@@ -11,32 +12,33 @@ class Compteur extends Component {
         compteur : 0
     }
 
-     calcul = (action) => {
-
-        if(action === "plus1"){
-            this.setState( ( statePrec ) => { return { compteur: statePrec.compteur + 1 } } )
-        } 
-        if(action === "moins1"){
-            this.setState( ( statePrec ) => { return { compteur: statePrec.compteur - 1 } } )
-        }
-    
-        
-    }
-    
-
     render() {
         return (
             <div className="cont">
             <div className="blocConmpt">
-                <Resultat valeur={this.state.compteur} />
+                <Resultat valeur={this.props.ctr} />
             </div>
             <div className="contBtn">
-                <IncrBtn txt="Incremente" clicked={() => this.calcul("plus1")} />
-                <IncrBtn txt="Decremente" clicked={() => this.calcul("moins1")} />
+                <IncrBtn txt="Incremente" clicked={this.props.incremente} />
+                <IncrBtn txt="Decremente" clicked={this.props.decremente} />
             </div>
         </div>
         )
     }
 }
 
-export default Compteur;
+const mapStateToProps = state => {
+    return {
+        ctr: state.compteur
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        incremente : () => dispatch({type: 'INCREMENTE'}),
+        decremente : () => dispatch({type: 'DECREMENTE'})
+
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Compteur);
